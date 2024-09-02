a80532d24455b71365bb25abac5d840b1c1ae4d9
Jakov <qwerasdftzu@gmail.com>
2024-09-02 16:46:07 +0200

    Add a bit of validation to Register page

--- Register/register.js
+++ Register/register.js
@@ -1,5 +1,30 @@
 const registerForms = document.querySelectorAll(".register-form");
+const confirmationCodeElement = document.querySelector(".confirmation-code");
+const resendConfirmationCodeBtn = document.querySelector(
+  ".resend-confirmation-code"
+);
 let step = 1;
+function handleUsernameAlert(message) {
+  if (!message) {
+    return registerForms[0].classList.remove("username-alert");
+  }
+  registerForms[0].classList.add("username-alert");
+  const usernameAlertParagraph = document.querySelector(".username-alert-p");
+  usernameAlertParagraph.textContent = message;
+}
+function handlePasswordAlert(message) {
+  if (!message) {
+    return registerForms[0].classList.remove("password-alert");
+  }
+  registerForms[0].classList.add("password-alert");
+  const passwordAlertParagraph = document.querySelector(".password-alert-p");
+  passwordAlertParagraph.textContent = message;
+}
+function resetValues(submitBtn) {
+  handlePasswordAlert("");
+  handleUsernameAlert("");
+  confirmationCodeElement.value = "";
+}
 function addStepToBody() {
   const body = document.body;
 
@@ -11,12 +36,45 @@ function addStepToBody() {
     body.classList.add("step1");
   }
 }
+function checkIfStep1IsValid() {
+  const registerForm = registerForms[0];
+  const usernameInput = registerForm.querySelector(".username-input");
+  const passwordInput = registerForm.querySelector(".password-input");
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  usernameInput.value = username.trim();
+  if (username.length < 3) {
+    handleUsernameAlert("Username must be at least 3 characters long");
+    return false;
+  }
+  if (username.length > 16) {
+    handleUsernameAlert("Username must be at most 16 characters long");
+    return false;
+  }
+  if (password.length < 8) {
+    handlePasswordAlert("Password must be at least 8 characters long");
+    return false;
+  }
+  return true;
+}
 function main() {
   const currentRegisterForm = registerForms[step - 1];
   const currentSubmitBtn = currentRegisterForm.querySelector(".submit-btn");
+  resetValues(currentSubmitBtn);
+
+  document.addEventListener("keydown", (event) => {
+    if (step !== 1) return;
+
+    if (checkIfStep1IsValid()) {
+      handleUsernameAlert("");
+      handlePasswordAlert("");
+      return currentSubmitBtn.classList.add("possible");
+    }
+    currentSubmitBtn.classList.remove("possible");
+  });
   currentSubmitBtn.addEventListener("click", () => {
     step++;
-    if (step > registerForms.length) {
+    if (step >= registerForms.length) {
       step = 1;
     }
     addStepToBody();
