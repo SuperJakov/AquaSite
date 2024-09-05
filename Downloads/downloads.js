19a68adc381db9fa7690ce011b931ac69bae93e9
Borisbobi1234 <at.gt.n5100@gmail.com>
2024-09-05 07:32:22 +0300

    Small bugfixes.

--- Downloads/downloads.js
+++ Downloads/downloads.js
@@ -22,6 +22,6 @@ search();
 phoneNum = null;
 function more(phone) {
   phone = phoneNum;
-  window.location.repace("./More/index.html");
+  window.open("./More/index.html");
   export {phoneNum};
 }
